6ad15a2f5e965f49b5d317bdbf3d17d8bf7e03a9
Tyson <100465135+DieMantra@users.noreply.github.com>
2024-06-13 15:20:34 +1000

    added build_client script, updated restart_server > deploy_server script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: "tic-tac-toe-server",
-      script: "./server/src/index.js",
+      script: "./server/dist/server/src/index.js",
       instances: 1,
       max_memory_restart: "300M",
 
       // Logging
-      out_file: "./out.log",
-      error_file: "./error.log",
+      out_file: "./server-out.log",
+      error_file: "./serve-error.log",
       merge_logs: true,
       log_date_format: "DD-MM HH:mm:ss Z",
       log_type: "json",
