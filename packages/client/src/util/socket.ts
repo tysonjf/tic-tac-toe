21e7f9a42456c3c28145d8f540a407ef28c112ca
Tyson <100465135+DieMantra@users.noreply.github.com>
2024-06-20 08:48:36 +1000

    slight bug in socket.ts

--- packages/client/src/util/socket.ts
+++ packages/client/src/util/socket.ts
@@ -1,8 +1,5 @@
 import { io, Socket } from "socket.io-client";
-import {
-  ServerToClientEvents,
-  ClientToServerEvents,
-} from "@shared/socketTypes";
+import { ClientToServerEvents, ServerToClientEvents } from "@tic-tac-toe/shared/socketTypes";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = import.meta.env.PROD
   ? import.meta.env.API_URL
